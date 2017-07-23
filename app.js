3fd332cb7b98525a49716b660cdbf40b6a767cfa
Ardzii <ardziig@gmail.com>
2017-07-23 12:34:16 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+const KrakenClient = require('@warren-bank/node-kraken-api')
+const kraken = new KrakenClient('api_key', 'api_secret', {timeout: 10000})
+
+
+var url = 'mongodb://localhost:27017/mongoTuto';
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected to the server.");
+  insertDocuments(db, function() {
+      db.close();
+  });
+
+});
+
+var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertOne({
+    ticker: kraken.api('Ticker', {"pair": 'XXBTZEUR'})
+                  .then((result) => {
+                    result
+                  })
+                  .catch((error) => {
+                    console.log('Error:', error.message)
+                  }),
+    time: 'today',
+  }, function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted documents into the collection");
+    callback(result);
+  });
+}
