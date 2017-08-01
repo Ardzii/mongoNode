94446b917de16821589688833317c4bf2c82b48e
Ardzii <ardziig@gmail.com>
2017-08-01 09:15:13 +0200

    Working on adding server timeStamp. Initial commit before modif to the core.

---
+++ modules/getServerTime.js
@@ -0,0 +1,11 @@
+var getFromApi = require('./getFromApi').getFromApi;
+
+function getServerTime() {
+  return getFromApi('Time').then((res) => {
+    console.log(JSON.stringify(res));
+  }).catch((error) => {
+    console.log('[ERROR]: ', error);
+  });
+}
+
+getServerTime();
